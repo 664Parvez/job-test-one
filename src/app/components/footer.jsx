9932ea7f2af070d277b345197fce417fd98550eb
664Parvez <parvezrabbi664@gmail.com>
2025-07-18 14:39:44 +0600

    first

---
+++ src/app/components/footer.jsx
@@ -0,0 +1,98 @@
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import FooterCss from "../styles/footer.module.css"
+
+import Logo from "../../../public/website-logo.png"
+
+import { MdArrowOutward, MdLocalPhone, MdOutlineEmail } from "react-icons/md";
+import { FaMapMarkerAlt, FaFacebook, FaInstagram, FaLinkedin, FaYoutube, FaShippingFast, FaCreditCard } from "react-icons/fa";
+import { BiSupport } from "react-icons/bi";
+import { RiMoneyPoundCircleLine } from "react-icons/ri";
+
+const footer = () => {
+
+    const date = new Date().getFullYear()
+
+  return (
+    <>
+        <footer>
+
+            {/* Copywrite */}
+            <div id={FooterCss.main_footer}>
+              <div className="container">
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-lg-4 col-md-6">
+                            <div id={FooterCss.contactInfo}>
+                                <Image src={Logo} width={0} height={0} layout='responsive' alt=''></Image>
+                                <div className="my-4">
+                                    <ul>
+                                        <li><MdLocalPhone /> 315-666-6688</li>
+                                        <li><MdOutlineEmail /> themesflat@gmail.com</li>
+                                        <li><FaMapMarkerAlt /> 549 Oak St.Crystal Lake, IL 60014</li>
+                                    </ul>
+                                </div>
+
+                                <div className="mt-4">
+                                    <Link href="/contact-us" id={FooterCss.getDirec}>Get Direction <MdArrowOutward /></Link>
+                                </div>
+
+                                <div id={FooterCss.socialMedia} className='mt-4'>
+                                    <ul>
+                                        <li><Link href=""><FaFacebook /></Link></li>
+                                        <li><Link href=""><FaInstagram /></Link></li>
+                                        <li><Link href=""><FaLinkedin /></Link></li>
+                                        <li><Link href=""><FaYoutube /></Link></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-lg-4 col-md-6">
+                            <div>
+                                <h5>Infomation</h5>
+                                <hr />
+                                <ul id={FooterCss.infoItem}>
+                                    <li><Link href="">Home</Link></li>
+                                    <li><Link href="">About Us</Link></li>
+                                    <li><Link href="">New Arrivals</Link></li>
+                                    <li><Link href="">My Account</Link></li>
+                                    <li><Link href="">Size Guide</Link></li>
+                                    <li><Link href="">Blog</Link></li>
+                                    <li><Link href="">Contact Us</Link></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="col-lg-4 col-md-6">
+                            <div>
+                                <h5>Customer Services</h5>
+                                <hr />
+                                <ul id={FooterCss.infoCustomer}>
+                                    <li><Link href="">Shipping</Link></li>
+                                    <li><Link href="">Orders FAQs</Link></li>
+                                    <li><Link href="">Returns & Refund</Link></li>
+                                    <li><Link href="">Privacy Policy</Link></li>
+                                    <li><Link href="">Terms & Conditions</Link></li>
+                                    <li><Link href="">Disclaimer</Link></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+              </div>
+            </div>
+            {/* Copywrite */}
+
+            {/* Copywrite */}
+            <div id={FooterCss.copywrite}>
+                <div id={FooterCss.copyText} className='text-center'>
+                  <p className='mb-0'>© {date} Fabrics. | All Right Reserved | Design & Development by: <Link href="https://parvez-rabbi.vercel.app/">Parvez Rabbi</Link></p>
+                </div>
+            </div>
+            {/* Copywrite */}
+        </footer>
+    </>
+  )
+}
+
+export default footer
--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,312 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import Link from "next/link";
+import homeStyles from "./page.module.css";
+
+// Icons
+import { LuBrain } from "react-icons/lu";
+import { FaCode, FaMicrophoneAlt } from "react-icons/fa";
+import { IoMdColorPalette } from "react-icons/io";
+import { MdArrowOutward } from "react-icons/md";
+
+// Images
+import hero from "../../public/hero.png"
+import hero2 from "../../public/hero2.jpg"
+import t1 from "../../public/t1.jpg"
+import t2 from "../../public/t2.jpg"
+import t3 from "../../public/t3.jpg"
+import t4 from "../../public/t4.jpg"
+import t5 from "../../public/t5.jpg"
+import t6 from "../../public/t6.jpg"
+import b1 from "../../public/b1.jpg"
+import b2 from "../../public/b2.jpg"
+import b3 from "../../public/b3.jpg"
+
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+      <section>
+        <div id={homeStyles.hero}>
+          <div className="container">
+            <div className="row align-items-center">
+              <div className="col-lg-5">
+                <h1>We Create Websites</h1>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt ducimus enim harum officia quis doloremque culpa eaque nihil? Quos ratione odio illum error consequuntur temporibus eius enim quam, rerum asperiores veritatis ab aliquam assumenda voluptatem esse dolores debitis neque et.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos temporibus iure blanditiis tempora!</p>
+
+                <div className="mt-4">
+                  <Link href="/portfolio" className="btn btn-lg">View Portfolio</Link>
+                </div>
+              </div>
+              <div className="col-lg-7">
+                <div id={homeStyles.hero_section_image}>
+                  <Image src={hero} width={0} height={0} className={homeStyles.hero_img_one} layout="responsive" alt=""></Image>
+                  <Image src={hero2} width={0} height={0} className={homeStyles.hero_img_two} layout="responsive" alt=""></Image>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </section>
+
+      <section>
+        <div id={homeStyles.second_section}>
+          <div className="container">
+            <div className="text-center" id={homeStyles.section_title}>
+              <h2>Epic Design and Development</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia hic facere quod in earum iure. Error exercitationem eligendi consequatur velit! Error exercitationem eligendi consequatur.</p>
+            </div>
+
+            <div className="row mt-5">
+              <div className="col-lg-3 col-md-6 mt-3">
+                <div id={homeStyles.secon_section_four_box} className="text-center">
+                  <LuBrain id={homeStyles.second_section_icon} />
+                  <h4>Strategy</h4>
+                  <p className="mb-0">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque dolorum eos perspiciatis officiis repudiandae esse deserunt.</p>
+                </div>
+              </div>
+              <div className="col-lg-3 col-md-6 mt-3">
+                <div id={homeStyles.secon_section_four_box} className="text-center">
+                  <FaCode id={homeStyles.second_section_icon} />
+                  <h4>Branding</h4>
+                  <p className="mb-0">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque dolorum eos perspiciatis officiis repudiandae esse deserunt.</p>
+                </div>
+              </div>
+              <div className="col-lg-3 col-md-6 mt-3">
+                <div id={homeStyles.secon_section_four_box} className="text-center">
+                  <FaMicrophoneAlt id={homeStyles.second_section_icon} />
+                  <h4>Development</h4>
+                  <p className="mb-0">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque dolorum eos perspiciatis officiis repudiandae esse deserunt.</p>
+                </div>
+              </div>
+              <div className="col-lg-3 col-md-6 mt-3">
+                <div id={homeStyles.secon_section_four_box} className="text-center">
+                  <IoMdColorPalette id={homeStyles.second_section_icon} />
+                  <h4>Web Design</h4>
+                  <p className="mb-0">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque dolorum eos perspiciatis officiis repudiandae esse deserunt.</p>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div id={homeStyles.third_section}>
+          <div className="container">
+            <div className="text-center" id={homeStyles.section_title}>
+                <h2>Portfolio</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia hic facere quod Doloremque dolorum eos perspiciatis officiis in earum iure. Error exercitationem eligendi consequatur adipisicing elit. Quia hic facere quod in earum iure velit!</p>
+            </div>
+
+            <div className="row">
+              <div className="col-lg-4 col-md-6 mt-3">
+                <Image src={t1} width={0} height={0} layout="responsive" alt=""></Image>
+                <div id={homeStyles.thirdTitleBox} className="text-center">
+                  <h4>Website Design</h4>
+                </div>
+              </div>
+              <div className="col-lg-4 col-md-6 mt-3">
+                <Image src={t2} width={0} height={0} layout="responsive" alt=""></Image>
+                <div id={homeStyles.thirdTitleBox} className="text-center">
+                  <h4>Mobile Application</h4>
+                </div>
+              </div>
+              <div className="col-lg-4 col-md-6 mt-3">
+                <Image src={t3} width={0} height={0} layout="responsive" alt=""></Image>
+                <div id={homeStyles.thirdTitleBox} className="text-center">
+                  <h4>Corporate Design</h4>
+                </div>
+              </div>
+              <div className="col-lg-4 col-md-6 mt-3">
+                <Image src={t4} width={0} height={0} layout="responsive" alt=""></Image>
+                <div id={homeStyles.thirdTitleBox} className="text-center">
+                  <h4>App Development</h4>
+                </div>
+              </div>
+              <div className="col-lg-4 col-md-6 mt-3">
+                <Image src={t5} width={0} height={0} layout="responsive" alt=""></Image>
+                <div id={homeStyles.thirdTitleBox} className="text-center">
+                  <h4>Responsive Design</h4>
+                </div>
+              </div>
+              <div className="col-lg-4 col-md-6 mt-3">
+                <Image src={t6} width={0} height={0} layout="responsive" alt=""></Image>
+                <div id={homeStyles.thirdTitleBox} className="text-center">
+                  <h4>Digital Product</h4>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div id={homeStyles.brand_value_section}>
+          <div className="container">
+            <div id={homeStyles.brand_value_full}>
+                <div id={homeStyles.brand_value_box}>
+                  <div className="text-center">
+                    <h2>Brand Values</h2>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae facilis aperiam ab nesciunt numquam nobis nisi.</p>
+                    <div className="mt-4">
+                      <Link href="/about-us" className="btn">Learn More</Link>
+                    </div>
+                  </div>
+                </div>
+                <div className="row align-items-center">
+                  <div className="col-lg-6">
+                    <div className="my-2">
+                      <Image src={b2} width={0} height={0} layout="responsive" className={homeStyles.tab_hide_img} id={homeStyles.mobile_hide_img} alt=""></Image>
+                    </div>
+                    <div className="my-2">
+                      <Image src={b3} width={0} height={0} layout="responsive" alt=""></Image>
+                    </div>
+                  </div>
+                  <div className="col-lg-6">
+                    <div className="my-2">
+                      <Image src={b1} width={0} height={0} layout="responsive" className={homeStyles.tab_hide_img} alt=""></Image>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+        </div>
+      </section>
+
+      <section>
+        <div id={homeStyles.forth_section}>
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-6 col-md-4"></div>
+              <div className="col-lg-6 col-md-8">
+                <h2>Construction and Renovation Projects</h2>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste, aliquam! Impedit debitis eveniet, veniam asperiores alias molestiae adipisci rem veritatis repellat velit tempora dolorem eum doloribus in ut quibusdam facere voluptatum dicta nemo! Non laudantium eum sint, vero at obcaecati labore laboriosam assumenda eius repellat.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, laborum. Eius, veritatis similique? Quisquam, ea?</p>
+
+                <Link href="/contact" className="btn btn-lg">Contact Us</Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div id={homeStyles.fifth_section}>
+          <div className="container">
+            <div className="text-center" id={homeStyles.section_title}>
+                <h2>What We Do ?</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia hic facere quod Doloremque dolorum eos perspiciatis officiis in earum iure. Error exercitationem eligendi consequatur adipisicing elit. Quia hic facere quod in earum iure velit!</p>
+            </div>
+
+            <div className="row mt-5">
+              <div className="col-lg-4">
+                <h4>Stratrgy.</h4>
+                <hr />
+                <ul className="mt-3">
+                  <li><MdArrowOutward /> <Link href="">Analytics and Research</Link></li>
+                  <li><MdArrowOutward /> <Link href="">Interactive Workshops</Link></li>
+                  <li><MdArrowOutward /> <Link href="">Brand Strategy</Link></li>
+                  <li><MdArrowOutward /> <Link href="">Content Strategy</Link></li>
+                  <li><MdArrowOutward /> <Link href="">Digital Strategy</Link></li>
+                  <li><MdArrowOutward /> <Link href="">Conversion Rate Optimization</Link></li>
+                </ul>
+              </div>
+              <div className="col-lg-4">
+                <h4>Design.</h4>
+                <hr />
+                <ul className="mt-3">
+                  <li><MdArrowOutward /> <Link href="">Creative Direction</Link></li>
+                  <li><MdArrowOutward /> <Link href="">Brand Guides</Link></li>
+                  <li><MdArrowOutward /> <Link href="">Prototypes</Link></li>
+                  <li><MdArrowOutward /> <Link href="">UI/UX & Web Design</Link></li>
+                  <li><MdArrowOutward /> <Link href="">Visual Asset Creation</Link></li>
+                  <li><MdArrowOutward /> <Link href="">Motion Design</Link></li>
+                </ul>
+              </div>
+              <div className="col-lg-4">
+                <h4>Development.</h4>
+                <hr />
+                <ul className="mt-3">
+                  <li><MdArrowOutward /> <Link href="">System Architecture Design</Link></li>
+                  <li><MdArrowOutward /> <Link href="">Full Stack Development</Link></li>
+                  <li><MdArrowOutward /> <Link href="">3rd Party Intergration</Link></li>
+                  <li><MdArrowOutward /> <Link href="">Perfomence Optimization</Link></li>
+                  <li><MdArrowOutward /> <Link href="">WordPress Development</Link></li>
+                  <li><MdArrowOutward /> <Link href="">Shopify Development</Link></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div id={homeStyles.sixth_section}>
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-7 col-md-6"></div>
+              <div className="col-lg-5 col-md-6">
+                <div id={homeStyles.six_section_boxs} className="my-4">
+                  <h3>Branding Strategy</h3>
+                  <p className="mb-0">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus, quia cum eius dignissimos at eaque doloremque, praesentium et ab culpa sed mollitia officiis deserunt consequatur?</p>
+
+                  <div className="mt-4">
+                    <Link href="">Read More <MdArrowOutward /></Link>
+                  </div>
+                </div>
+                <div id={homeStyles.six_section_boxs} className="my-4">
+                  <h3>Designing Logos</h3>
+                  <p className="mb-0">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus, quia cum eius dignissimos at eaque doloremque, praesentium et ab culpa sed mollitia officiis deserunt consequatur?</p>
+                </div>
+                <div id={homeStyles.six_section_boxs} className="my-4">
+                  <h3>Branding Identity</h3>
+                  <p className="mb-0">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus, quia cum eius dignissimos at eaque doloremque, praesentium et ab culpa sed mollitia officiis deserunt consequatur?</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div id={homeStyles.seventh_section}>
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-5 col-md-6">
+                <div id={homeStyles.seventh_section_box}>
+                  <h3>Crafting Digital Experiences</h3>
+                  <h5>Join the elit ranks of sustained value creators</h5>
+                  <p className="mt-4">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores dolores tempore amet architecto consequatur illum officia numquam iusto eligendi omnis.</p>
+                  <Link href="/contact-us" className="btn btn-lg"> Contact Us </Link>
+                </div>
+              </div>
+              <div className="col-lg-7 col-md-6"></div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div id={homeStyles.eight_section}>
+          <div className="container">
+            <div className="row align-items-center">
+              <div className="col-lg-6">
+                <h3>Subscribe to Our Newsletter</h3>
+                <p className="mb-0">Sign up for our newsletter and get 10% off your first purchase. Get it Now quickly</p>
+              </div>
+              <div className="col-lg-6">
+                <form action="">
+                  <input type="email" placeholder="Enter your email . . ." className="form-control form-control-lg" />
+                  <button className="btn btn-lg mt-3">Subscribe</button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
